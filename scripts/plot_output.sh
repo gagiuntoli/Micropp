ea192bf1fa77524c059084547d5706917817cdc1
GG1991 <giuntoli1991@gmail.com>
2018-06-05 13:25:27 +0200

    printing fields of the script

--- scripts/plot_output.sh
+++ scripts/plot_output.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
+echo "# t_step nr_its nr_tol epsilon sig11 sig22 ... sig13" > aux1.dat
+
 awk '
 /Time step/ {printf("%d ", $4)}
 /NEWTON-R/ {printf("%d %e ", $4, $7)} 
 /e11/ {printf("%e ", $3)}
 /Average stress/ {printf("%e %e %e %e %e %e\n", $4, $5, $6, $7, $8, $9);getline;} 
-' $1 > data.dat
+' $1 > aux2.dat
 
-
+cat aux1.dat aux2.dat > data.dat
+rm aux1.dat aux2.dat
 
 #Time step = 58
 #NewRap It = 4 Tol = 7.99515
