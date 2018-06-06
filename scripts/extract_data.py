8c0029115f2a0cd84469f2deeaa83c9bd4e33b63
GG1991 <giuntoli1991@gmail.com>
2018-06-06 11:15:19 +0200

    reading the number of vtu files automatically

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,12 +1,13 @@
 from paraview.simple import *
 import numpy as np
+import glob
 
-n_pvtu_files = 99;
+n_pvtu_files = tifCounter = len(glob.glob1("./","*.vtu"))
 displ_y = np.zeros((n_pvtu_files,1))
 force_y = np.zeros((n_pvtu_files,1))
 alldata = np.zeros((n_pvtu_files,2))
 
-for i in range(1, n_pvtu_files):
+for i in range(0, n_pvtu_files):
 #for i in range(54, 55):
 
     file_in = "micropp_1_"+str(i)+".vtu"
@@ -15,10 +16,12 @@ for i in range(1, n_pvtu_files):
     except IOError:
       print " error, not found"
       sys.exit()
+
+    print "processing : ",file_in
     
     VTU  = XMLUnstructuredGridReader(FileName=file_in)
-    KEYs = VTU.GetPointDataInformation().keys(); print KEYs;
-    KEYs = VTU.GetCellDataInformation().keys(); print KEYs;
+    KEYs = VTU.GetPointDataInformation().keys(); #print KEYs;
+    KEYs = VTU.GetCellDataInformation().keys();  #print KEYs;
     
     slice1 = Slice(Input=VTU)
     slice1.SliceType = 'Plane'
@@ -34,7 +37,7 @@ for i in range(1, n_pvtu_files):
     sigyy_wall = DataSliceFile.GetCellData().GetArray('stress').GetValue(1)
     displ_y[i] = DataSliceFile.GetPointData().GetArray('displ').GetValue(1)
     force_y[i] = sigyy_wall*area
-    print area, sigyy_wall, displ_y[i], force_y[i]
+    #print area, sigyy_wall, displ_y[i], force_y[i]
 
 #----------
 
