a969249ea44cdfe99fa1e8eefe2227675a1cb369
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2018-07-05 23:35:03 +0200

    Changes in the micro constructor.
    Some internal variables were declared constant for security.

--- src/micro.cpp
+++ src/micro.cpp
@@ -20,12 +20,50 @@
 
 #include <vector>
 #include <iostream>
+
+#include <cassert>
+
 #include "micro.hpp"
 
-micropp_t::micropp_t(const int dim, const int size[3], const int micro_type, const double *micro_params,
-		          const int *mat_types, const double *params)
+micropp_t::micropp_t(const int _dim, const int size[3], const int _micro_type,
+                     const double *_micro_params, const int *mat_types, const double *params):
+	dim(_dim),
+
+	lx(_micro_params[0]),
+	ly(_micro_params[1]),
+	lz(_micro_params[2]),
+
+	inv_tol(micro_params[4]),
+	nx(size[0]),
+	ny(size[1]),
+	nz(_dim == 2 ? 1 : size[2]),
+	nn(nx * ny * nz),
+
+	dx(lx / (nx - 1)),
+	dy(ly / (ny - 1)),
+	dz(lz / (nz - 1)),
+
+	npe(dim == 2 ? 4 : 8),
+	nvoi(dim == 2 ? 3 : 6),
+	nelem(dim == 2 ? (nx - 1) * (ny - 1) : (nx - 1) * (ny - 1) * (nz - 1)),
+
+	micro_type(_micro_type),
+	num_int_vars(nelem * 8 * NUM_VAR_GP)
 {
-	this->micro_type = micro_type;
+	assert(dim == 2 || dim == 3);
+
+
+	b = (double *)malloc(nn * dim * sizeof(double));
+	du = (double *)malloc(nn * dim * sizeof(double));
+	u = (double *)malloc(nn * dim * sizeof(double));
+	elem_stress = (double *)malloc(nelem * nvoi * sizeof(double));
+	elem_strain = (double *)malloc(nelem * nvoi * sizeof(double));
+	elem_type = (int *)malloc(nelem * sizeof(int));
+	vars_old = (double *)malloc(num_int_vars * sizeof(double));
+	vars_new = (double *)malloc(num_int_vars * sizeof(double));
+
+	assert( b && du && u && elem_stress && elem_strain &&
+	        elem_type && vars_old && vars_new );
 
 	int nParams;
 	if (micro_type == 0) {
@@ -40,28 +78,22 @@ micropp_t::micropp_t(const int dim, const int size[3], const int micro_type, con
 		nParams = 5;
 	}
 
-	for (int i = 0; i < nParams; i++) {
-		this->micro_params[i] = micro_params[i];
-	}
-	lx = this->micro_params[0];
-	ly = this->micro_params[1];
-	lz = this->micro_params[2];
-
-	inv_tol = this->micro_params[4];
+	for (int i = 0; i < nParams; i++)
+		micro_params[i] = _micro_params[i];
 
 	ofstream file;
 	file.open("micropp_materials.dat");
 	file << scientific;
-
 	for (int i = 0; i < numMaterials; i++) {
-		material_list[i].E = params[i * MAX_MAT_PARAM + 0];
+		material_list[i].E  = params[i * MAX_MAT_PARAM + 0];
 		material_list[i].nu = params[i * MAX_MAT_PARAM + 1];
 		material_list[i].Sy = params[i * MAX_MAT_PARAM + 2];
 		material_list[i].Ka = params[i * MAX_MAT_PARAM + 3];
 
 		material_list[i].k = material_list[i].E / (3 * (1 - 2 * material_list[i].nu));
 		material_list[i].mu = material_list[i].E / (2 * (1 + material_list[i].nu));
-		material_list[i].lambda = (material_list[i].nu * material_list[i].E) / ((1 + material_list[i].nu) * (1 - 2 * material_list[i].nu));	// lambda
+		material_list[i].lambda = (material_list[i].nu * material_list[i].E) /
+			((1 + material_list[i].nu) * (1 - 2 * material_list[i].nu));	// lambda
 
 		if (mat_types[i] == 0) {
 			// lineal
@@ -76,48 +108,10 @@ micropp_t::micropp_t(const int dim, const int size[3], const int micro_type, con
 			material_list[i].plasticity = false;
 			material_list[i].damage = true;
 		}
-		file << setw(14) << material_list[i].E << " " << material_list[i].nu << " " << material_list[i].Sy << " " << material_list[i].Ka << endl;
+		file << setw(14) << material_list[i].E << " " << material_list[i].nu
+		     << " " << material_list[i].Sy << " " << material_list[i].Ka << endl;
 	}
 	file.close();
-
-	this->dim = dim;
-	if (dim == 2) {
-		nx = size[0];
-		ny = size[1];
-		nvoi = 3;
-		nn = nx * ny;
-		npe = 4;
-	} else if (dim == 3) {
-		nx = size[0];
-		ny = size[1];
-		nz = size[2];
-		nvoi = 6;
-		nn = nx * ny * nz;
-		npe = 8;
-	}
-
-	dx = lx / (nx - 1);
-	dy = ly / (ny - 1);
-	if (dim == 2) {
-		nelem = (nx - 1) * (ny - 1);
-	} else if (dim == 3) {
-		dz = lz / (nz - 1);
-		nelem = (nx - 1) * (ny - 1) * (nz - 1);
-	}
-
-	num_int_vars = nelem * 8 * NUM_VAR_GP;
-
-	b = (double *)malloc(nn * dim * sizeof(double));
-	du = (double *)malloc(nn * dim * sizeof(double));
-	u = (double *)malloc(nn * dim * sizeof(double));
-	elem_stress = (double *)malloc(nelem * nvoi * sizeof(double));
-	elem_strain = (double *)malloc(nelem * nvoi * sizeof(double));
-	elem_type = (int *)malloc(nelem * sizeof(int));
-	vars_old = (double *)malloc(num_int_vars * sizeof(double));
-	vars_new = (double *)malloc(num_int_vars * sizeof(double));
-
-	for (int i = 0; i < nn * dim; i++)
-		u[i] = 0.0;
 
 	if (dim == 2) {
 		for (int ex = 0; ex < nx - 1; ex++) {
@@ -126,7 +120,8 @@ micropp_t::micropp_t(const int dim, const int size[3], const int micro_type, con
 				elem_type[e] = get_elem_type(ex, ey);
 			}
 		}
-	} else {
+		ell_init_2D(&A, dim, nx, ny);
+	} else if (dim == 3) {
 		for (int ex = 0; ex < nx - 1; ex++) {
 			for (int ey = 0; ey < ny - 1; ey++) {
 				for (int ez = 0; ez < nz - 1; ez++) {
@@ -135,12 +130,8 @@ micropp_t::micropp_t(const int dim, const int size[3], const int micro_type, con
 				}
 			}
 		}
-	}
-
-	if (dim == 2)
-		ell_init_2D(&A, dim, nx, ny);
-	else if (dim == 3)
 		ell_init_3D(&A, dim, nx, ny, nz);
+	}
 
 	calc_ctan_lin();
 
@@ -157,6 +148,7 @@ micropp_t::micropp_t(const int dim, const int size[3], const int micro_type, con
 micropp_t::~micropp_t()
 {
 	ell_free(&A);
+
 	free(b);
 	free(du);
 	free(u);
@@ -175,15 +167,17 @@ micropp_t::~micropp_t()
 void micropp_t::get_nl_flag(int gp_id, int *non_linear)
 {
 	*non_linear = 0;
-	for (auto const &gp:gauss_list)
+	for (auto const &gp : gauss_list)
 		if (gp.id == gp_id) {
-			*non_linear = (gp.int_vars_n == NULL) ? 0 : 1;
+			*non_linear = (gp.int_vars_n != NULL);
 			break;
 		}
 }
 
 int micropp_t::get_elem_type(int ex, int ey)
 {
+	assert(micro_type == 0 || micro_type == 1);
+
 	if (micro_type == 0) {
 		// esfera en matriz
 		double x1 = ex * dx + dx / 2;
@@ -198,11 +192,15 @@ int micropp_t::get_elem_type(int ex, int ey)
 		double espesor = micro_params[3];
 		return (y < espesor);
 	}
-	return 0;
+
+	cerr << "Invalid micro_type = " << micro_type << endl;
+	return -1;
 }
 
 int micropp_t::get_elem_type(int ex, int ey, int ez)
 {
+	assert(micro_type == 0 || micro_type == 1);
+
 	if (micro_type == 0) {
 		// esfera en matriz
 		double x1 = ex * dx + dx / 2;
@@ -222,5 +220,6 @@ int micropp_t::get_elem_type(int ex, int ey, int ez)
 		return (y < espesor);
 	}
 
-	return 0;
+	cerr << "Invalid micro_type = " << micro_type << endl;
+	return -1;
 }
