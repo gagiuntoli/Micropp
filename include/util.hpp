fe5b9e5d4f9c5fcf79b95906f89b9f4baf036b9e
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2018-07-09 16:38:20 +0200

    Improved the instrumentation functions to use chronos.
    There are other small improves obly related with instrumentation.

--- include/util.hpp
+++ include/util.hpp
@@ -40,46 +40,15 @@
 
 using namespace std;
 
-inline double getus(const struct timespec &ts)
+inline uint64_t devest(const vector<uint64_t>  &in, const uint64_t mean)
 {
-	return ts.tv_sec * 1000000.0 + double(ts.tv_nsec) * 1.0E-3;
-}
-
-inline double devest(const vector<struct timespec>  &in, double mean)
-{
-	double out = 0;
+	uint64_t out = 0;
 	for (const auto &x : in) {
-		const double tmp = (getus(x) - mean);
+		const uint64_t tmp = (x - mean);
 		out += tmp * tmp;
 	}
 
-	out /= in.size();
-
-	return sqrt(out);
-}
-
-inline void getTime(struct timespec &ts)
-{
-	const int rc = clock_gettime(CLOCK_MONOTONIC, &ts);
-	if (rc) {
-		std::cerr << "Error reading time: " << strerror(errno) << std::endl;
-		exit(1);
-	}
-}
-
-static inline struct timespec getDt(const struct timespec &t1,
-                                    const struct timespec &t2)
-{
-	struct timespec dt;
-
-	if (t2.tv_nsec >= t1.tv_nsec) {
-		dt.tv_sec = t2.tv_sec - t1.tv_sec;
-		dt.tv_nsec = t2.tv_nsec - t1.tv_nsec;
-	} else {
-		dt.tv_sec = t2.tv_sec - 1 - t1.tv_sec;
-		dt.tv_nsec = 1000000000L + t2.tv_nsec - t1.tv_nsec;
-	}
-	return dt;
+	return sqrt(out / in.size());
 }
 
 #endif //UTIL_HPP
--- src/instrument.cpp
+++ src/instrument.cpp
@@ -25,50 +25,56 @@
 
 using namespace std;
 
+atomic<size_t> instrument::instances(0);
+uint64_t instrument::initialTime(0);
 unordered_map<string, timevect> instrument::times;                   // Final register time
-thread_local unordered_map<string, timestack> instrument::startTime; // function stack calls
-struct timespec instrument::initialTime = {0};
-atomic<int> instrument::instances(0);
 
-void instrument::init()
+void instrument::initialize()
 {
-	if (instances++)
-		getTime(initialTime);
+	if (!instances++)
+		initialTime = take_time_stamp();
 }
 
 void instrument::finalize()
 {
 	if (0 == --instances) {
-		struct timespec finalTime;
-		getTime(initialTime);
+		const uint64_t elapsed = (take_time_stamp() - initialTime) * 1E-3;
 
 		size_t cont = 0;
-		cout << "# Final execution report" << endl;
+		cout << "# Final execution report: total time = " << elapsed << endl;
+		cout << setw(6) << left << "# No"
+		     << setw(20) << "function"
+		     << setw(8) << right << "calls"
+		     << setw(16) << "total time"
+		     << setw(16) << "percent"
+		     << setw(16) << "mean"
+		     << setw(16) << "stdev"
+		     << setw(16) << "relative"
+		     << endl;
+
+		cout.precision(2);
+		cout << fixed;
 
 		for (auto const &f : times) {
-			const double total = accumulate(f.second.begin(), f.second.end(),
-			                                0.0,
-			                                [](double x, const struct timespec &ts) {
-				                                return x + getus(ts);
-			                                });
+			const uint64_t total = accumulate(f.second.begin(),
+			                                  f.second.end(),
+			                                  0.0);
 			const size_t entries = f.second.size();
-			const double mean = total / entries;
+			const uint64_t mean = total / entries;
+			const double percent = double(total) * 100 / elapsed;
+			const uint64_t stdev = devest(f.second, mean);
+			const double relative = double(stdev) * 100 / mean;
 
-			cout << cont++ << " "
-			     << setw(20) << left
-			     << f.first << " "
-			     << entries << " "
-			     << total << " "
-			     << mean << " "
-			     << devest(f.second, mean)
+			cout << setw(6) << left << cont++
+			     << setw(20) << f.first
+			     << setw(8) << right << entries
+			     << setw(16) << total
+			     << setw(16) << percent
+			     << setw(16) << mean
+			     << setw(16) << stdev
+			     << setw(16) << relative
 			     << endl;
-
-			if (!startTime[f.first].empty())
-				cout << "# " << startTime[f.first].size()
-				     << " Unpaired entries for: " << f.first << endl;
 		}
-
-		cout << "Total Time: " << getus(getDt(initialTime, finalTime)) << endl;
 	}
 }
 
