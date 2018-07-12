291909f0ffda878fe6f379736b27ce57ff6360e0
Guido Giuntoli <gagiuntoli@gmail.com>
2018-07-12 11:27:29 +0200

    setting private to functions

--- include/micro.hpp
+++ include/micro.hpp
@@ -114,76 +114,56 @@ class micropp_t {
 		                          {+CONSTXG, +CONSTXG, +CONSTXG},
 		                          {-CONSTXG, +CONSTXG, +CONSTXG} };
 
-	public:
-		micropp_t(const int dim, const int size[3], const int micro_type,
-		          const double *micro_params, const int *mat_types,
-		          const double *params);
-
-		~micropp_t();
-
 		void calc_ctan_lin();
-
 		bool is_linear(const double *macro_strain);
-
 		double get_inv_1(const double *tensor);
 		double get_inv_2(const double *tensor);
-
-		void set_macro_strain(const int gp_id, const double *macro_strain);
-		void get_macro_stress(const int gp_id, double *macro_stress);
-		void get_macro_ctan(const int gp_id, double *macro_ctan);
-
-		void homogenize();
-		void update_vars();
-		void get_nl_flag(int gp_id, int *nl_flag);
-
 		void set_displ(double *eps);
 		double assembly_rhs(bool *nl_flag);
-
+		void assembly_mat();
 		void get_elem_rhs2D(int ex, int ey, bool *nl_flag, double (&be)[2 * 4]);
 		void get_elem_rhs3D(int ex, int ey, int ez, bool *nl_flag, double (&be)[3 * 8]);
-
-		void assembly_mat();
-
 		void get_elem_mat2D(int ex, int ey, double (&Ae)[2 * 4 * 2 * 4]);
 		void get_elem_mat3D(int ex, int ey, int ez, double (&Ae)[3 * 8 * 3 * 8]);
-
 		void solve();
 		void newton_raphson(bool *nl_flag, int *its, double *err);
-
 		void get_ctan_plast_sec(int ex, int ey, int ez, int gp, double ctan[6][6]);
 		void get_ctan_plast_exact(int ex, int ey, int ez, int gp, double ctan[6][6]);
 		void get_ctan_plast_pert(int ex, int ey, int ez, int gp, double ctan[6][6]);
-
 		void get_strain2D(int ex, int ey, int gp, double *strain_gp);
 		void get_strain3D(int ex, int ey, int ez, int gp, double *strain_gp);
-
 		void get_stress2D(int ex, int ey, int gp, double strain_gp[3],
 		                  bool *nl_flag, double *stress_gp);
-
 		void get_stress3D(int ex, int ey, int ez, int gp, double strain_gp[3],
 		                  bool *nl_flag, double *stress_gp);
-
 		void get_dev_tensor(double tensor[6], double tensor_dev[6]);
 		void plastic_step(const material_t *material, double eps[6],
 		                  double eps_p_1[6], double alpha_1,
 		                  double eps_p[6], double *alpha,
 		                  bool *nl_flag, double stress[6]);
-
 		void getElemDisp(int ex, int ey, double *elem_disp);
 		void getElemDisp(int ex, int ey, int ez, double *elem_disp);
-
 		int get_elem_type2D(int ex, int ey);
 		int get_elem_type3D(int ex, int ey, int ez);
-
 		material_t get_material(const int e);
-
 		void calc_bmat_3D(int gp, double bmat[6][3 *8]);
-
 		void calc_fields();
 		void calc_ave_stress(double stress_ave[6]);
 		void calc_ave_strain(double strain_ave[6]);
-
-		void output(int tstep, int gp_id);
 		void write_vtu(int tstep, int gp_id);
+
+	public:
+
+		micropp_t(const int dim, const int size[3], const int micro_type, const double *micro_params,
+		          const int *mat_types, const double *params);
+		~micropp_t();
+
+		void set_macro_strain(const int gp_id, const double *macro_strain);
+		void get_macro_stress(const int gp_id, double *macro_stress);
+		void get_macro_ctan(const int gp_id, double *macro_ctan);
+		void homogenize();
+		void update_vars();
+		void get_nl_flag(int gp_id, int *nl_flag);
+		void output(int tstep, int gp_id);
 		void write_info_files();
 };
