c03223037477d3a2098c7f76d6fa786ee4223572
Guido Giuntoli <gagiuntoli@gmail.com>
2018-08-02 16:47:20 +0200

    Some modification on mesh convergence script

--- scripts/convergence.sh
+++ scripts/convergence.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# Mesh converge test
+#
+# Uses test3d_1 : 3D 2-Layers microstructure
+# with Mat1 ISOLIN and Mat2 Plastic.
+#
+# Increases the size as nx = ny = nz = nn
+#
+# Arrangement in micropp output
+# epsxx [1] # epsyy [2] # epszz[3] # epsxy[4]  # epsxz[5]  # epsyz[6]
+# sigxx [7] # sigyy [8] # sigzz[9] # sigxy[10] # sigxz[11] # sigyz[12]
+#
+
+nn_size=( 2 3 4 5 6 7 8 9 10 )
+ts=130
+dires=( 0 1 2 3 4 5 )
 
 EXEC=../../build_release/test/test3d_1 
 
@@ -14,66 +30,27 @@ echo $out_folder
 mkdir $out_folder
 cd $out_folder
 
-function generic {
-
-# Increases the size independently as nx , ny , nz
-
-nx_size=( 2 4 12 )
-ny_size=( 2 4 12 16 )
-nz_size=( 2 4 )
-dir=1 # sigxx[0] sigyy[1] sigzz[2] sigxy[3] sigxz[4] sigyz[5] 
-
-for nx in ${nx_size[@]}; do 
- for ny in ${ny_size[@]}; do 
-  for nz in ${nz_size[@]}; do 
-   echo $nx $ny $nz
-   $EXEC $nx $ny $nz $dir 130 > out.dat
-   awk '/eps/{printf("%lf ", $3)} /sig/{printf("%lf\n", $(3+dir))}' out.dat > s_vs_e_${nx}_${ny}_${nz}.dat
-  done
- done
-done
-
-echo "set term png ; set output \"curves.png\";" > file
-echo "plot \\" >> file
-
-for nx in ${nx_size[@]}; do 
- for ny in ${ny_size[@]}; do 
-  for nz in ${nz_size[@]}; do 
-   echo "\"s_vs_e_${nx}_${ny}_${nz}.dat\" u 1:2 w lp title '$nx-$ny-$nz',\\" >> file
-  done
- done
-done
-
-#gnuplot file
-
-}
-
-function uniform {
-
-# Increases the size as nx = ny = nz = nn
-
-nn_size=( 2 4 6 8 )
-dires=( 0 1 2 3 4 5 ) # sigxx[0] sigyy[1] sigzz[2] sigxy[3] sigxz[4] sigyz[5] 
-
 for d in ${dires[@]}; do
+  
+  rm -f max_${d}.dat 
   for nn in ${nn_size[@]}; do 
      echo "dir = " $d " nn = " $nn
-     $EXEC $nn $nn $nn $d 100 > /dev/null
-     mv micropp_eps_sig_ctan.dat micropp_epssig_${nn}.dat
+     $EXEC $nn $nn $nn $d $ts > /dev/null
+     awk -v d_a=$d 'BEGIN{getline;}{print $(d_a + 1) "\t" $(d_a + 7) }' \
+                micropp_eps_sig_ctan.dat > sig_vs_eps_${d}_${nn}.dat
+     sed '$!d' sig_vs_eps_${d}_${nn}.dat | awk -v n_a=$nn '{print n_a "\t" $2}' >> max_${d}.dat
   done
 
-  echo "set term png ; set output \"curves_$d.png\";" > gnuplot.in
+  echo "set term png;" > gnuplot.in
+  echo "set output \"curves_${d}.png\";" >> gnuplot.in
   echo "plot \\" >> gnuplot.in
 
   for nn in ${nn_size[@]}; do 
-     echo "\"micropp_epssig_${nn}.dat\" u $(($d+1)):$(($d+7)) w lp title '${nn}',\\" >> gnuplot.in
+     echo "\"sig_vs_eps_${d}_${nn}.dat\" u 1:2 w lp title '${nn}',\\" \
+     >> gnuplot.in
   done
 
   gnuplot gnuplot.in
 done
 
-}
-
-#generic
-uniform
 cd -
--- test/test3d_1.cpp
+++ test/test3d_1.cpp
@@ -50,8 +50,8 @@ int main (int argc, char *argv[])
 	double micro_params[5] = { 1., 1., 1., .5, 0. };
 
 	material_t mat_params[2];
-	mat_params[0].set(1.0e6, 0.25, 1.0e2, 2.0e5, 1);
-	mat_params[1].set(1.0e6, 0.25, 1.0e2, 2.0e5, 0);
+	mat_params[0].set(1.0e6, 0.25, 1.0e5, 2.0e5, 1);
+	mat_params[1].set(1.0e6, 0.25, 1.0e5, 2.0e5, 0);
 
 	micropp<3> micro(1, size, micro_type, micro_params, mat_params);
 
@@ -62,9 +62,9 @@ int main (int argc, char *argv[])
 
 		cout << "time step = " << t << endl;
 
-		if (t < 20)
+		if (t < 80)
 			eps[dir] += D_EPS;
-		else if (t < 40)
+		else if (t < 160)
 			eps[dir] -= D_EPS;
 		else
 			eps[dir] += D_EPS;
