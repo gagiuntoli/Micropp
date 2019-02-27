4e95e3233684b0eab7699acb0fd3ce1704f30094
Guido Giuntoli <gagiuntoli@gmail.com>
2019-02-28 00:03:40 +0100

    Modification on scripts

--- scripts/plot_omp.sh
+++ scripts/plot_omp.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-N=30
-NGP=400
+. vars.sh
 
-folder="res_${N}_${NGP}"
 
 procs=(1 2 4 8 16 32 48)
 
-rm -rf times_n${N}_ngp${NGP}.dat
-t1=$(awk '/time =/{print $3}' ${folder}/res_1.dat)
+for N in ${sizes[@]}; do
 
-for i in ${procs[@]}; do
+	folder="res_${N}_${NGP}"
+	rm -rf times_n${N}_ngp${NGP}.dat
+	t1=$(awk '/time =/{print $3}' ${folder}/res_1.dat)
 
-	awk -v t1=$t1 -v p=$i '/time =/{print p "\t" $3 "\t" (t1/$3)}' ${folder}/res_${i}.dat >> times_n${N}_ngp${NGP}.dat
+	for i in ${procs[@]}; do
 
+		awk -v t1=$t1 -v p=$i '/time =/{print p "\t" $3 "\t" (t1/$3)}' ${folder}/res_${i}.dat >> times_n${N}_ngp${NGP}.dat
+
+	done
 done
