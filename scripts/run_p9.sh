99e90118252c0330ff4374242c16a6a0caf0bfdc
Guido Giuntoli <gagiuntoli@gmail.com>
2019-05-09 14:03:48 +0200

    Small changes in example

--- scripts/run_p9.sh
+++ scripts/run_p9.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-#SBATCH --job-name="test"
+#SBATCH --job-name="micropp"
 #SBATCH --workdir=.
-#SBATCH --output=test_%j.out
-#SBATCH --error=test_%j.err
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=160
-### #SBATCH --nodes=1
-### #SBATCH --ntasks-per-node=4
-### #SBATCH --qos=bsc_case
-####SBATCH --qos=debug
 #SBATCH --gres=gpu:4
 #SBATCH --exclusive
 #SBATCH --time=00:05:00
 
-EXEC=""
+### #SBATCH --nodes=1
+### #SBATCH --ntasks-per-node=4
+### #SBATCH --qos=bsc_case
+### #SBATCH --qos=debug
 
-export OMP_NUM_THREADS=1
+N=50
+NGP=1
+
+EXEC="../build-gpu/test/test3d_2"
+
+export OMP_NUM_THREADS=4
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
-time mpirun -np 1 $EXEC
+#time mpirun -np 1 $EXEC 100 1 20
+time nvprof -o nvprof-profile-${N}-${NGP}.out $EXEC $N $NGP 20
+#time $EXEC 50 4 20
