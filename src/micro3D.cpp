fc75c8976b571a8e71d281efa0f58f4f9765388a
Guido Giuntoli <gagiuntoli@gmail.com>
2020-01-13 22:42:08 +0100

    WIP: Pulling appart common functions from OpenACC and CPU-only

--- src/micro3D.cpp
+++ src/micro3D.cpp
@@ -2,10 +2,7 @@
  *  This source code is part of MicroPP: a finite element library
  *  to solve microstructural problems for composite materials.
  *
- *  Copyright (C) - 2018 - Jimmy Aguilar Mena <kratsbinovish@gmail.com>
- *                         Guido Giuntoli <gagiuntoli@gmail.com>
- *                         Judicaël Grasset <judicael.grasset@stfc.ac.uk>
- *                         Alejandro Figueroa <afiguer7@maisonlive.gmu.edu>
+ *  Copyright (C) - 2018
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,6 +17,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 #include "micropp.hpp"
 #include "common.hpp"
@@ -142,113 +140,3 @@ void micropp<3>::calc_bmat(int gp, double bmat[nvoi][npe * dim]) const
 		bmat[5][i * dim + 2] = dsh[i][1];
 	}
 }
-
-
-template<>
-double micropp<3>::assembly_rhs(const double *u,
-				const double *int_vars_old,
-				double *b)
-{
-	INST_START;
-
-	memset(b, 0., nndim * sizeof(double));
-
-	double be[dim * npe];
-	int index[dim * npe];
-
-	for (int ez = 0; ez < nez; ++ez) {
-		for (int ey = 0; ey < ney; ++ey) {
-			for (int ex = 0; ex < nex; ++ex) {
-
-				int n[npe];
-				get_elem_nodes(n, nx, ny, ex, ey, ez);
-
-				for (int j = 0; j < npe; ++j)
-					for (int d = 0; d < dim; ++d)
-						index[j * dim + d] = n[j] * dim + d;
-
-				get_elem_rhs(u, int_vars_old, be, ex, ey, ez);
-
-				for (int i = 0; i < npe * dim; ++i)
-					b[index[i]] += be[i];
-			}
-		}
-	}
-
-	// boundary conditions
-	for (int i = 0; i < nx; ++i) {
-		for (int j = 0; j < ny; ++j) {
-			const int n = nod_index3D(i, j, 0); // z = 0
-			memset(&b[n * dim], 0., dim * sizeof(double));
-		}
-	}
-
-	for (int i = 0; i < nx; ++i) {
-		for (int j = 0; j < ny; ++j) {
-			const int n = nod_index3D(i, j, nz - 1); // z = lx
-			memset(&b[n * dim], 0., dim * sizeof(double));
-		}
-	}
-
-	for (int i = 0; i < nx; ++i) {
-		for (int k = 1; k < nz - 1; ++k) {
-			const int n = nod_index3D(i, 0, k); // y = 0
-			memset(&b[n * dim], 0., dim * sizeof(double));
-		}
-	}
-
-	for (int i = 0; i < nx; ++i) {
-		for (int k = 1; k < nz - 1; ++k) {
-			const int n = nod_index3D(i, ny - 1, k); // y = ly
-			memset(&b[n * dim], 0., dim * sizeof(double));
-		}
-	}
-
-	for (int j = 1; j < ny - 1; ++j) {
-		for (int k = 1; k < nz - 1; ++k) {
-			const int n = nod_index3D(0, j, k); // x = 0
-			memset(&b[n * dim], 0., dim * sizeof(double));
-		}
-	}
-
-	for (int j = 1; j < ny - 1; j++) {
-		for (int k = 1; k < nz - 1; ++k) {
-			const int n = nod_index3D(nx - 1, j, k); // x = lx
-			memset(&b[n * dim], 0., dim * sizeof(double));
-		}
-	}
-
-	// Common
-	for (int i = 0; i < nndim; ++i)
-		b[i] = -b[i];
-
-	double norm = 0.0;
-	for (int i = 0; i < nndim; ++i)
-		norm += b[i] * b[i];
-	norm = sqrt(norm);
-
-	return norm;
-}
-
-
-template <>
-void micropp<3>::assembly_mat(ell_matrix *A, const double *u, const double *int_vars_old)
-{
-	INST_START;
-
-	ell_set_zero_mat(A);
-
-	double Ae[npe * dim * npe * dim];
-	for (int ex = 0; ex < nex; ++ex) {
-		for (int ey = 0; ey < ney; ++ey) {
-			for (int ez = 0; ez < nez; ++ez) {
-				get_elem_mat(u, int_vars_old, Ae, ex, ey, ez);
-				ell_add_3D(A, ex, ey, ez, Ae);
-			}
-		}
-	}
-	ell_set_bc_3D(A);
-}
-
-
-template class micropp<3>;
--- src/openacc/ell.cpp
+++ src/openacc/ell.cpp
@@ -2,10 +2,7 @@
  *  This source code is part of MicroPP: a finite element library
  *  to solve microstructural problems for composite materials.
  *
- *  Copyright (C) - 2018 - Jimmy Aguilar Mena <kratsbinovish@gmail.com>
- *                         Guido Giuntoli <gagiuntoli@gmail.com>
- *                         Judicaël Grasset <judicael.grasset@stfc.ac.uk>
- *                         Alejandro Figueroa <afiguer7@maisonlive.gmu.edu>
+ *  Copyright (C) - 2018
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
